eb11e6f5f8e42ae76380d27a6739146ab0c91b28
Michał Warda <michal.warda@elpassion.pl>
2018-10-04 11:01:45 +0200

    Add subscribing to stream of ticks

--- src/looper/Looper.test.js
+++ src/looper/Looper.test.js
@@ -1,10 +1,17 @@
+import xs from 'xstream'
+
 class Timer {
   constructor(bpm) {
     this.bpm = bpm;
+    this.stream = xs.periodic((1000 * this.bpm / 60) / 256);
   }
 
   setBpm(newBpm) {
     this.bpm = newBpm;
+  }
+
+  subscribe(fn) {
+    this.stream.addListener({ next: fn });
   }
 }
 
@@ -18,5 +25,14 @@ describe("Timer", () => {
     const timer = new Timer(120);
     timer.setBpm(240);
     expect(timer.bpm).toBe(240);
+  });
+
+  test("publishes an event every second for bpm = 60 * 256", () => {
+    jest.useFakeTimers();
+    const ticks = [];
+    const timer = new Timer(60);
+    timer.subscribe(event => ticks.push(event))
+    jest.advanceTimersByTime(1000);
+    expect(ticks.length).toBe(256);
   })
 });
