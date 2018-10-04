676faeb38910c08ab2360964b43df684d880a5b5
Michał Warda <michal.warda@elpassion.pl>
2018-10-04 14:48:18 +0200

    Add eventStart, Stop for Track

--- src/looper/Track.js
+++ src/looper/Track.js
@@ -1,16 +1,49 @@
+import xs from "xstream";
+
 export default class Track {
   static emptyArray(length) {
     return Array.from({ length }, () => []);
   }
 
   events = Track.emptyArray(1024);
+  currentlyRecordingNotes = {};
 
   constructor($timer) {
-    this.$stream = $timer.map(step => this.events[step]).filter((events) => events.length);
+    this.$timer = $timer;
+    this.$eventStream = {
+      start: listener => {
+        this.eventListener = listener;
+      },
+      stop: () => {}
+    };
+    xs.combine(this.$eventStream, this.$timer).map(
+      ([{ type, ...event }, step]) => {
+        if (type === "START") {
+          this.currentlyRecordingNotes[event.note] = {
+            ...event,
+            startStep: step
+          };
+        } else {
+          const note = this.currentlyRecordingNotes[event.note];
+          this.addEvent(note.step, { ...note, endStep: step });
+        }
+      }
+    );
+    this.$stream = this.$timer
+      .map(step => this.events[step])
+      .filter(events => events.length);
   }
 
   addEvent(step, event) {
     this.events[step].push(event);
+  }
+
+  addEventStart(note, velocity) {
+    this.eventListener.next({ type: "START", note, velocity });
+  }
+
+  addEventStop(note) {
+    this.eventListener.next({ type: "STOP", note });
   }
 
   clear() {
