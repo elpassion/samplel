7c8c06022737b656737a8760612c147b6d265a5b
Michał Warda <michal.warda@elpassion.pl>
2018-10-04 10:39:09 +0200

    add setBpm

--- src/looper/Looper.test.js
+++ src/looper/Looper.test.js
@@ -2,11 +2,21 @@ class Timer {
   constructor(bpm) {
     this.bpm = bpm;
   }
+
+  setBpm(newBpm) {
+    this.bpm = newBpm;
+  }
 }
 
 describe("Timer", () => {
-  test("sets correct bpm", () => {
+  test("sets initial bpm", () => {
     const timer = new Timer(120);
     expect(timer.bpm).toBe(120);
+  });
+
+  test("sets new bpm", () => {
+    const timer = new Timer(120);
+    timer.setBpm(240);
+    expect(timer.bpm).toBe(240);
   })
-})
+});
